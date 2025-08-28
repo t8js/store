e0f7f34efdc5eb621df6a9768e2d683fcfa8c821
axtk <axtkx@inbox.ru>
2025-08-29 01:41:18 +0300

    add revision

--- src/Store.ts
+++ src/Store.ts
@@ -3,10 +3,10 @@ export type StoreUpdateCallback<T> = (nextState: T, prevState: T) => void;
 
 export class Store<T> {
     state: T;
-    callbacks: StoreUpdateCallback<T>[];
+    callbacks: StoreUpdateCallback<T>[] = [];
+    revision = 0;
     constructor(data: T) {
         this.state = data;
-        this.callbacks = [];
     }
     onUpdate(callback: StoreUpdateCallback<T>) {
         this.callbacks.push(callback);
@@ -26,6 +26,10 @@ export class Store<T> {
             update instanceof Function ? update(this.state) : update;
 
         this.state = nextState;
+
+        if (this.revision === Number.MAX_SAFE_INTEGER)
+            this.revision = 1;
+        else this.revision++;
 
         for (let callback of this.callbacks) callback(nextState, prevState);
     }

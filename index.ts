ef70dcd99c3b1994ead657f30c357ca920210fc8
axtk <axtkx@inbox.ru>
2025-12-05 14:48:08 +0300

    add persist util

--- index.ts
+++ index.ts
@@ -1,2 +1,3 @@
 export * from "./src/isStore.ts";
+export * from "./src/persist.ts";
 export * from "./src/Store.ts";
---
+++ src/persist.ts
@@ -0,0 +1,57 @@
+import { Store } from "./Store.ts";
+
+function getStorage(session: boolean) {
+  if (typeof window === "undefined") return;
+  return session ? sessionStorage : localStorage;
+}
+
+/**
+ * Allows to make the given `store`'s state persistent across page reloads.
+ *
+ * The store state is saved to and restored from the given `storageKey`
+ * either of `localStorage` if the `session` parameter is `false` (which is
+ * the default), or of `sessionStorage` if `session` is set to `true`.
+ * Interaction with the browser storage is skipped in non-browser environments.
+ *
+ * Returns the original store.
+ * 
+ * @example
+ * ```js
+ * let counterStore = persist(new Store(0), "counter");
+ * counterStore.emit("sync"); // signals to read from localStorage now
+ * ```
+ */
+export function persist<T>(store: Store<T>, storageKey: string, session = false) {
+  let inited = false;
+
+  function read() {
+    let storage = getStorage(session);
+
+    if (storage) {
+      try {
+        let rawState = storage.getItem(storageKey);
+        if (rawState !== null) store.setState(JSON.parse(rawState) as T);
+      }
+      catch {}
+    }
+
+    if (!inited) inited = true;
+  }
+
+  function write(state: T) {
+    let storage = getStorage(session);
+
+    if (inited && storage) {
+      try {
+        storage.setItem(storageKey, JSON.stringify(state));
+      }
+      catch {}
+    }
+  }
+
+  store.on("sync", read);
+  store.once("effect", read);
+  store.on("update", write);
+
+  return store;
+}

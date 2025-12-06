07dc4db447b9766d6de1d72819140b166cde3e65
axtk <axtkx@inbox.ru>
2025-12-06 20:52:26 +0300

    update tests

--- tests.ts
+++ tests.ts
@@ -14,10 +14,10 @@ let store = new Store(10);
 
 let testValue = [100, -3];
 let unsubscribe = [
-  store.on("update", (state) => {
+  store.onUpdate((state) => {
     testValue[0] += state;
   }),
-  store.on("update", (state) => {
+  store.onUpdate((state) => {
     testValue[1] *= state;
   }),
 ];
@@ -26,7 +26,7 @@ assert(isStore(store), true);
 assert(isStore({}), false);
 
 assert(store.getState(), 10);
-assert(store.callbacks.update.size, 2);
+assert(store.callbacks.size, 2);
 
 store.setState(2);
 assert(store.getState(), 2);
@@ -39,7 +39,7 @@ assert(testValue[0], 77);
 assert(testValue[1], 150);
 
 unsubscribe[1]();
-assert(store.callbacks.update.size, 1);
+assert(store.callbacks.size, 1);
 
 store.setState(12);
 assert(store.getState(), 12);

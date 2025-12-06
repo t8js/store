525a5188908d53b64351e3587e966baec6ffa4a2
axtk <axtkx@inbox.ru>
2025-12-06 21:06:43 +0300

    lint

--- src/isPersistentStore.ts
+++ src/isPersistentStore.ts
@@ -1,5 +1,5 @@
 import { isStore } from "./isStore.ts";
-import { PersistentStore } from "./PersistentStore.ts";
+import type { PersistentStore } from "./PersistentStore.ts";
 
 export function isPersistentStore<T>(x: unknown): x is PersistentStore<T> {
   return isStore(x) && "sync" in x;

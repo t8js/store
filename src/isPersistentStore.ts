993e5349927d56cb1aec5636e5c3b92504851971
axtk <axtkx@inbox.ru>
2025-12-06 21:13:14 +0300

    update isPersistentStore

--- src/isPersistentStore.ts
+++ src/isPersistentStore.ts
@@ -2,5 +2,5 @@ import { isStore } from "./isStore.ts";
 import type { PersistentStore } from "./PersistentStore.ts";
 
 export function isPersistentStore<T>(x: unknown): x is PersistentStore<T> {
-  return isStore(x) && "sync" in x;
+  return isStore<T>(x) && "sync" in x;
 }
